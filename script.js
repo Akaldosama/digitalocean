371991bd840a85522558a70a1137c43b3ed062fb
acer <you@example.com>
2023-10-24 14:08:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// let random = Math.floor(Math.random() * 10 + 1)
+
+
+// let arr = [random,2,4,5]
+// console.log(arr)
+
+//     if(arr % 2 == 0){
+//         arr.splice(0,1,arr[0]+1)
+//     }else{
+//         arr.splice(0,1,arr[0] +1)
+//     }
+
+// console.log(arr)
+
+
+
+// function findLargestNumber(arr) {
+//     if (arr.length === 0) {
+//       return undefined; 
+//     }
+  
+//     let max = arr[0]; 
+  
+//     for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//         max = arr[i];
+//       }
+//     }
+  
+//     return max;
+//   }
+  
+//   const numbers = [12, 5, 8, 42, 3, 19];
+//   const largestNumber = findLargestNumber(numbers);
+//   console.log("The largest number is: " + largestNumber);
+
+
+
+
+// let arr = [-1,0,-3,4,5,-2,3,1,-5]
+// console.log(arr)
+// let arrMinus = []
+
+// for (let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//         arrMinus.push(arr[i])
+//     }
+// }
+// console.log(arrMinus)
+
+
+
+
+// teskar array             
+
+// let arr = [1,27,6]
+
+// for( let i = arr.length - 1; i >= 0; i -= 1){
+//  console.log(arr[i])
+// }
